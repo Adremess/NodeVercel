a5ad723af2e46af6a5ab82fb6b1733a9453b9ab0
Adrian <esteban.adrian.gonz@gmail.com>
2022-11-26 22:48:26 -0300

    reset

--- api/pages/Register.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const register = require('../controllers/Register.Controller');
-const router = Router();
-const validationMiddleware = require('../middlewares/validationMiddleware');
-const registerValidation = require('../helpers/schemasValidation/registerValidation');
-
-router.post('/register', validationMiddleware(registerValidation), register);
-
-module.exports = router;
--- src/database/database.js
+++ src/database/database.js
@@ -8,7 +8,6 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectModule: require('mysql2')
     // dialectOptions: {
     //   ssl: {
     //     require: true,
